ba22edf275825a2171e82c08269373c1f86b2906
wermuind <wermuind@gmail.com>
2024-10-20 13:38:50 +0300

    Update notes

--- notes/assets/js/5e95c892.ebf7abcc.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunknotes=self.webpackChunknotes||[]).push([[9661],{1892:(e,s,r)=>{r.r(s),r.d(s,{default:()=>o});r(7294);var n=r(6905),t=r(1944),u=r(5281),a=r(8790),c=r(6040),l=r(5893);function o(e){return(0,l.jsx)(t.FG,{className:(0,n.Z)(u.k.wrapper.docsPages),children:(0,l.jsx)(c.Z,{children:(0,a.H)(e.route.routes)})})}}}]);
